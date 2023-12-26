390c52e9ec5842cf1128973f20a94f97671e33a6
Abesse Smahi <contact@smahi.net>
2023-12-26 12:09:11 +0100

    Add comments

--- 013_csv.py
+++ 013_csv.py
@@ -1,5 +1,8 @@
 import csv
 
+# you can download the sample csv file in Linux OS using the following command
+# wget https://github.com/smahi/python-for-beginners/raw/main/users_data.csv
+
 with open('users_data.csv') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
