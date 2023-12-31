3235fca1cca15005cc935a701b8608b4cb5a0851
Abesse Smahi <contact@smahi.net>
2023-12-31 13:06:39 +0100

    Add notes

--- 015_mp3_downloader.py
+++ 015_mp3_downloader.py
@@ -1,6 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
+import eyed3
+import os
+from bs4 import BeautifulSoup   
 
+
+download_path = 'Downloads'
+
+if not os.path.exists(download_path):
+    os.mkdir(download_path)
+    
 
 
 # the script will try to download mp3 files from: https://server7.mp3quran.net/shur/
@@ -24,7 +32,25 @@ if(response.status_code == 200):
         urls.append(url)
         
         
-    print(urls[0])
+    for url in urls[-4:-1]:
+        print(f"Downloading: {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"{url} - downloaded.")
+            filename = url.split('/')[-1]
+            file_path = f"{download_path}/{filename}"
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+                audio_file = eyed3.load(file_path)
+                title = audio_file.tag.title
+                splitted_filename = filename.split('.')
+                new_filename = f"{splitted_filename[0]}-{title}.{splitted_filename[1]}"
+                new_file_path = f"{download_path}/{new_filename}"
+                print(f"Rename {file_path} to {new_file_path}")
+                os.rename(file_path, new_file_path)
+                
+        else:
+            print(f"Unable to download {url}")
 else:
     print("An error occurred.")
 
